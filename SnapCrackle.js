dece48a66273e4048735338de772fde2f2643dd3
WallaceAngelo <wallaceangelo@hotmail.com>
2021-08-17 18:59:59 -0300

    respondido 1

---
+++ SnapCrackle.js
@@ -0,0 +1,19 @@
+function  snapCrackle(maxValue){
+    let text = ""
+    for(let num = 1; num <= maxValue; num++){ 
+      if(num % 2 !== 0 && num % 5 === 0){
+      text += "SnapCrackle, ";
+        continue;
+      }
+    if(num % 2 !== 0){
+      text += "Snap, ";
+      continue;
+    }
+    if(num % 5 === 0){
+      text += "Crackle, ";
+      continue;  
+    }
+    text += num + ", ";
+  }
+  console.log(text)
+  }
